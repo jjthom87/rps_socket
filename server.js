8242fcfd32dd3e936761126e3eb39c613352a357
Jared Thomas <jjthom87@yahoo.com>
2018-05-02 18:51:06 -0400

    adding

--- server.js
+++ server.js
@@ -13,46 +13,35 @@ var PORT = process.env.PORT || 3000;
 app.use(express.static("./public"));
 io.on('connection', function (socket) {
   socket.on('firstPlayerInput', function(data){
-    console.log('firstPlayerInput: ' + socket.id);
     io.emit('firstPlayerClient', {data: data, id: socket.id});
   });
   socket.on('secondPlayerInput', function(data){
-    console.log('secondPlayerInput: ' + socket.id);
     io.emit('secondPlayerClient', {data: data, id: socket.id});
   });
   socket.on('playerOnePicked', function(data){
-    console.log('playerOnePicked: ' + socket.id);
     io.emit('playerOnePickedClient', {data: data, id: socket.id});
   });
   socket.on('playerTwoPicked', function(data){
-    console.log('playerTwoPicked: ' + socket.id);
     io.emit('playerTwoPickedClient', {data: data, id: socket.id});
   });
   socket.on('playerOnePick', function(data){
-    console.log('playerOnePick: ' + socket.id);
     io.emit('playerOnePickClient', {data: data, id: socket.id});
   });
   socket.on('playerTwoPick', function(data){
-    console.log('playerTwoPick: ' + socket.id);
     io.emit('playerTwoPickClient', {data: data, id: socket.id});
   });
   socket.on('playersPicked', function(data){
-    console.log('playersPicked: ' + socket.id);
     io.emit('playersPickedClient', {data: data, id: socket.id});  
   });
   socket.on('playersPickedOneFirst', function(data){
-    console.log('playersPickedOneFirst: ' + socket.id);
     io.emit('playersPickedOneFirstClient', {data: data, id: socket.id});  
   });
   socket.on('playersPickedTwoFirst', function(data){
-    console.log('playersPickedTwoFirst: ' + socket.id);
     io.emit('playersPickedTwoFirstClient', {data: data, id: socket.id});  
   });
   socket.on('disconnect', function(data){
-    console.log('disconnect: ' + socket.id);
     io.emit('playerDisconnected', socket.id);
   });
-
 });
 
 app.get("/", function(req, res) {
